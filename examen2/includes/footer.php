e5b608c1ec0cac63e4bc48adea923dbf959f5c6c
astroLyn <astrid4286@gmail.com>
2024-10-31 01:22:30 -0700

    Add files via upload

---
+++ examen2/includes/footer.php
@@ -0,0 +1,14 @@
+<section>
+        <footer>
+        <nav>
+                <a href="index.php">Home</a>
+                <a href="nosotros.php">Us</a>
+                <a href="">Advertisements</a>
+                <a href="blog.php">Blog</a>
+                <a href="contacto.php">Contact</a>
+            </nav>  
+            <p>All right reserved by: J. Arellano</p>
+        </footer>
+    </section>
+</body>
+</html>
---
+++ examen2/includes/headers.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="includes/styles.css">
+    <title>Home</title>
+</head>
+<body>
+    <section>
+        <h1>Real estate Ray</h1>
+        <header>
+            <nav>
+                <a href="index.php">Home</a>
+                <a href="nosotros.php">Us</a>
+                <a href="">Advertisements</a>
+                <a href="blog.php">Blog</a>
+                <a href="contacto.php">Contact</a>
+            </nav>
+        </header>
+        <h2>Sale of luxury houses and apartments</h2>
+    </section>
